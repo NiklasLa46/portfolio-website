4d54e6b2da25e3cbacb7cafd2fc529dc7bd1931e
Niklas Lampe <125186474+NiklasLa46@users.noreply.github.com>
2025-03-06 11:10:36 +0100

    atf, header and about me html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { AtfComponent } from './atf/atf/atf.component';
+import { AboutMeComponent } from "./about-me/about-me/about-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AtfComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/atf/atf/atf.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atf',
+  standalone: true,
+  imports: [],
+  templateUrl: './atf.component.html',
+  styleUrls: ['./atf.component.scss','../../../styles.scss']
+})
+export class AtfComponent {
+
+}
